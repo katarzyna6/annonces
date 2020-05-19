70e220cbefa8752d8423b0e41d19d8e03f774f89
unknown <katharina.r@hotmail.fr>
2020-05-19 08:13:12 +0200

    annonces

---
+++ views/membre.php
@@ -0,0 +1,24 @@
+<div class="espace">
+
+    <h1>Mon espace</h1>
+
+</div>
+
+<html>
+<body>
+
+    <div class = "form3">
+        <h2>Ajouter une annonce</h2>
+        
+            <form action="index.php?route=membre" method="POST">
+                
+                <label for="description"><input type="text" placeholder="Description" name="description"/></label>
+                <label for="date_limite"><input type="text" placeholder="Date limite" name="date_limite"/></label>
+                <input type="submit" value="Ajouter"/>
+                <h3><a href="index.php">Retour</a><h3>
+                <h3><a href="index.php?route=deconnect">Me déconnecter</a></h3>
+    
+            </form>
+    </div>
+</body>
+</html>
